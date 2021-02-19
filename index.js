56263878bb6fda88c9b89f1532f915f5700ea336
pratapshivendra817 <71538947+pratapshivendra817@users.noreply.github.com>
2021-02-19 13:31:00 +0530

    task

--- index.js
+++ index.js
@@ -7,26 +7,6 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
-
-
-  // Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
